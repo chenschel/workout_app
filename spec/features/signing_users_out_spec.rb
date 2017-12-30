2851af1a45365af0e0fa5834305b6fd87c6d1bf3
Christopher Henschel <christopher.henschel@sumcumo.com>
2017-12-30 21:01:50 +0100

    singing out an user

---
+++ spec/features/signing_users_out_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.feature 'Sign-out an user' do
+  before do
+    @john = User.create(email: 'john@example.com', password: 'password')
+
+    visit '/'
+    click_link('Sign in')
+    fill_in('Email', with: @john.email)
+    fill_in('Password', with: @john.password)
+    click_button('Log in')
+  end
+  scenario 'by click sign out link is successful' do
+    visit '/'
+    click_link('Sign out')
+
+    expect(page).to have_content('Signed out successfully')
+  end
+end
