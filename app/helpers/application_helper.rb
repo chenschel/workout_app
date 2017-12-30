d95586f5fb547f3bb3dc8ea3b845472e3a7451e3
Christopher Henschel <christopher.henschel@sumcumo.com>
2017-12-30 21:29:07 +0100

    stylize devise error messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def devise_error_messages!
+    return '' if resource.errors.empty?
+
+    # rubocop:disable UselessAssignment
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    # rubocop:enable UselessAssignment
+    html = <<-HTML
+      <div class="alert alert-error alert-danger">
+      <a href='#' class='close' data-dismiss='alert' &#215;></a>
+      <%= content_tag :div, messages if messages.is_a?(String) %>
+      </div>
+    HTML
+    html.html_safe
+  end
 end
