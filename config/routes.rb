ff22037f5e28598d0ec0a0d19decca135006a5cd
Christopher Henschel <christopher.henschel@sumcumo.com>
2018-01-01 21:35:57 +0100

    add exercise mordel include warden to rspec

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   get 'dashboard/index'
 
+  resources :users do
+    resources :exercises
+  end
+
   root to: 'dashboard#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
