af4e4aa7debf0f9167302014c816c8f679b961bc
Christopher Henschel <christopher.henschel@sumcumo.com>
2018-01-06 22:08:57 +0100

    Implement searching

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,12 @@ Rails.application.routes.draw do
     resources :exercises
   end
 
+  resource :dashboard, only: [:index] do
+    collection do
+      post :search, to: 'dashboard#search'
+    end
+  end
+
   root to: 'dashboard#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
