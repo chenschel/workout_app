119a0bf9fb724aa0abac091f9095f3cc86a3dff0
Christopher Henschel <christopher.henschel@sumcumo.com>
2018-01-06 21:28:11 +0100

    add pagination

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,6 @@
 # dashboard controller
 class DashboardController < ApplicationController
   def index
-    @athletes = User.all
-    @athletes = @athletes.reject { |user| user.id == current_user.id } if user_signed_in?
+    @athletes = User.paginate(page: params[:page])
   end
 end
