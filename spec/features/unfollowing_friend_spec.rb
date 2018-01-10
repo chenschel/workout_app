a99852ffc96a6942e5d1a0fec082554a19f799e4
Christopher Henschel <christopher.henschel@sumcumo.com>
2018-01-10 15:36:53 +0100

    refactor unfollow spec and use let

--- spec/features/unfollowing_friend_spec.rb
+++ spec/features/unfollowing_friend_spec.rb
@@ -1,24 +1,24 @@
 require 'rails_helper'
 
 RSpec.feature 'Unfollowing Friend' do
-  before do
-    @john = create(:user)
-    login_as(@john)
+  let(:john) { create(:user) }
+  let(:sarah) { create(:user, first_name: 'Sarah') }
 
-    @sarah = create(:user, first_name: 'Sarah')
-    @fiendship = Friendship.create(user: @john, friend: @sarah)
+  before do
+    login_as(john)
+    @fiendship = Friendship.create(user: john, friend: sarah)
   end
 
   scenario do
     visit('/')
 
     click_link('My Lounge')
-    expect(page).to have_content(@sarah.full_name)
+    expect(page).to have_content(sarah.full_name)
     link = "a[href='/friendships/#{@fiendship.id}'][data-method='delete']"
     find(link).click
 
-    expect(page.current_path).to eq(user_exercises_path(@john.id))
-    expect(page).to have_content("#{@sarah.full_name} unfollowed")
-    expect(page).not_to have_link(@sarah.full_name)
+    expect(page.current_path).to eq(user_exercises_path(john.id))
+    expect(page).to have_content("#{sarah.full_name} unfollowed")
+    expect(page).not_to have_link(sarah.full_name)
   end
 end
